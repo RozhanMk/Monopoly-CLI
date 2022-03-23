6b5043b0866c908f9e46723fb822f4ef6152be0e
Mohammad Nakhjiri <mohammaddev02@gmail.com>
2022-03-23 14:41:50 +0430

    reward onFieldActions added

--- src/Reward.java
+++ src/Reward.java
@@ -2,4 +2,10 @@ public class Reward extends Field{
     public Reward(int id, boolean colored) {
         super(id, colored);
     }
+
+    @Override
+    public void onFieldActions(Player player) {
+        super.onFieldActions(player);
+        player.increaseCash(200);
+    }
 }
