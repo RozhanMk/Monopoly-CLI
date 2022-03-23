7048611badc3f2cd7f26cf64ae8f9f63142612f9
Mohammad Nakhjiri <mohammaddev02@gmail.com>
2022-03-23 14:54:06 +0430

    Road onFieldAction added

--- src/Road.java
+++ src/Road.java
@@ -2,4 +2,10 @@ public class Road extends Field{
     public Road(int id, boolean colored) {
         super(id, colored);
     }
+
+    @Override
+    public void onFieldActions(Player player) {
+        super.onFieldActions(player);
+        player.decreaseCash(100);
+    }
 }
