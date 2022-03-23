db836bcdea8cc7aff63e4b49e2976d8b81de1b94
RozhanMk <rozhan.mirzaei82@gmail.com>
2022-03-23 13:23:20 +0430

    added cinema auto actions

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,5 +1,35 @@
 public class Cinema extends Invest{
     public Cinema(int id, boolean colored , ColorType color) {
         super(id, colored , color);
+        setCost(200);
+    }
+    public void autoActs(Player player){
+        if(!player.checkIsOwner(super.getId())){
+            Player owner = super.getOwner();
+            //paying fine
+            if(owner != null){
+                int countCinema = 0;
+                if(owner.checkIsOwner(14)){
+                    countCinema++;
+                }
+                if(owner.checkIsOwner(7)){
+                    countCinema++;
+                }
+                if(owner.checkIsOwner(3)){
+                    countCinema++;
+                }
+                if(countCinema == 1){
+                    setFine(25);
+                }
+                if(countCinema == 2){
+                    setFine(50);
+                }
+                if(countCinema == 3){
+                    setFine(100);
+                }
+                player.decreaseCash(getFine());
+            }
+    
+        }
     }
 }
